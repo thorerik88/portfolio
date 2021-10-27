3d7120c3970c5177e05997946813364b175ba370
thorerik88 <thorerik88@hotmail.com>
2021-10-27 11:33:49 +0200

    mobile menu dropdown working

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// mobile dropdown menu
+
+const mobileBtn = document.querySelector(".nav-hero__header__hamburger");
+const mobileMenu = document.querySelector(".dropdown");
+let icon = document.querySelector(".fa-bars");
+
+// open or close menu and change icon accordingly
+mobileBtn.addEventListener("click", function() {
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+        icon.classList = "fas fa-bars";
+    } else {
+        mobileMenu.style.display = "block";
+        icon.classList = "fas fa-times";
+    }
+})
+
+// close menu when link is clicked
+mobileMenu.addEventListener("click", function() {
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+        icon.classList = "fas fa-bars";
+    }
+})
