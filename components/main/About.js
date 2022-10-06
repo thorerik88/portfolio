173e7a6843907653562c207f783029bcb50450a2
Thor-Erik Stovland <thorerik88@hotmail.com>
2022-10-06 14:41:14 +0200

    changed some minor bugs

--- components/main/About.js
+++ components/main/About.js
@@ -14,7 +14,7 @@ const About = () => {
               <div className={styles.content}>
                 <h4>Heading</h4>
                 <p>Hi! My name is Thor-Erik and I enjoy to build stuff that ends up on the great web. My interest in coding started in 2016 and have since been my passion.</p>
-                <p>Now, I've just finished my studies and have been a part of various interesting projects. Anything from design/homepage to a bookting site.My focus lies with happy customers.</p>
+                <p>Now, I have just finished my studies and have been a part of various interesting projects. Anything from design/homepage to a bookting site.My focus lies with happy customers.</p>
                 <div className={styles.url}>
                     <Link href="https://github.com/thorerik88">
                       <a>My Github account!</a>
